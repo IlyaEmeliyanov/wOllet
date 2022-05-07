5fd458d0a69eebb276c5e8e9b72f2b1390f89af5
IlyaEmeliyanov <ilya.emeliyanov20@gmail.com>
2022-05-07 10:35:15 +0200

    Initial Commit

---
+++ PaymentApp/PaymentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentAppApp.swift
+//  PaymentApp
+//
+//  Created by Ilya Emeliyanov on 07/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PaymentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
